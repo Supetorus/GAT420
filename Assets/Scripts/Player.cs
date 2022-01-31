0fcf91ce135c1e20df5a784fb14940a472cbf943
Supetorus <66396555+Supetorus@users.noreply.github.com>
2022-01-31 11:08:06 -0700

    Start class 11/31

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,3 @@
-using Assets.Scripts;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -21,6 +18,6 @@ public class Player : MonoBehaviour
 		direction.z = Input.GetAxis("Vertical");
 
 		transform.position += direction * speed * Time.deltaTime;
-		transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
+		transform.position = Utilities.Wrap(transform.position, new Vector3(-20, -20, -20), new Vector3(20, 20, 20));
 	}
 }
--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -1,26 +1,45 @@
-﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Assets.Scripts
+public static class Utilities
 {
-	public static class Utilities
+    public static Vector3 Wrap(Vector3 v, Vector3 min, Vector3 max)
+    {
+        Vector3 result = v;
+
+        if (v.x > max.x) result.x = min.x + (max.x - v.x);
+        else if (v.x < min.x) result.x = max.x - (min.x - v.x);
+
+        if (v.y > max.y) result.y = min.y + (max.y - result.y);
+        else if (v.y < min.y) result.y = max.y - (min.y - result.y);
+
+        if (v.z > max.z) result.z = min.z + (max.z - result.z);
+        else if (v.z < min.z) result.z = max.z - (min.z - result.z);
+
+        return result;
+    }
+
+	public static bool ScreenToWorld(Vector2 screen, LayerMask layerMask, ref Vector3 position)
 	{
-		public static Vector3 Wrap(Vector3 v, Vector3 min, Vector3 max)
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out RaycastHit hitInfo, 100))
 		{
-			Vector3 result = v;
+			// check if hit layer mask
+			if (((1 << hitInfo.collider.gameObject.layer) & layerMask) == 0) return false;
 
-			if (result.x > max.x) result.x = min.x + (result.x - max.x);
-			if (result.x < min.x) result.x = max.x + (min.x - result.x);
-			if (result.y > max.y) result.y = min.y + (result.y - max.y);
-			if (result.y < min.y) result.y = max.y + (min.y - result.y);
-			if (result.z > max.z) result.z = min.z + (result.z - max.z);
-			if (result.z < min.z) result.z = max.z + (min.z - result.z);
-
-			return result;
+			position = hitInfo.point;
 		}
+
+		return true;
+	}
+
+	public static Vector3 SnapToGrid(Vector3 v, Vector3 grid)
+	{
+		v.x = (grid.x == 0) ? v.x : Mathf.RoundToInt(v.x / grid.x) * grid.x;
+		v.y = (grid.y == 0) ? v.y : Mathf.RoundToInt(v.y / grid.y) * grid.y;
+		v.z = (grid.z == 0) ? v.z : Mathf.RoundToInt(v.z / grid.z) * grid.z;
+
+		return v;
 	}
 }
